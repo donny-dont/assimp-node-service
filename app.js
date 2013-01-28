7831169f83459ecd254b7b0e2957019fc7b35650
Don Olmstead <don.j.olmstead@gmail.com>
2013-01-27 18:37:01 -0800

    Adding CORS support

--- app.js
+++ app.js
@@ -11,10 +11,25 @@ var express = require('express')
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(allowCrossDomain);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser({
